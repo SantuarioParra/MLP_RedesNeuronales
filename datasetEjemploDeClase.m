0c87140ec8bec55f15a92f906d512aac968d8f5a
EricMontesinosEspinosa <44953158+EricMontesinosEspinosa@users.noreply.github.com>
2018-11-18 22:02:16 -0600

    Add files via upload

---
+++ datasetEjemploDeClase.m
@@ -0,0 +1,18 @@
+fprintf('Para la funcón de clase\n');
+rangoInferior=input('limite inferior del rango\n');
+rangoSuperior=input('limite superior del rango\n');
+cs=input('cantidad de los saltos\n');
+
+idcE=fopen('nuevoCE.txt','w');
+intervalo=(rangoSuperior-rangoInferior)/cs;
+p=rangoInferior+intervalo;
+separador=1;
+i=1;
+while i<=cs
+            gdp=1+sin(((6*pi)/4)*p);
+            fprintf(idcE,'%f\t',p);
+            fprintf(idcE,'%f\r\n',gdp);   
+            p=p+intervalo;
+            i=i+1;
+end
+fclose(idcE);
